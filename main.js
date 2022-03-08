6ea928ea39279d873321af6febdf26ca948e649b
DFKorn <d1man2192@mail.ru>
2022-03-08 17:53:01 +0300

    add functionality for slideshow

---
+++ main.js
@@ -0,0 +1,29 @@
+let width = 31;
+let step = 1;
+let imgDisplayed = 3;
+
+
+let slide = document.querySelector('.slideshow');
+let prev = document.querySelector('.previous');
+let next = document.querySelector('.next');
+let locLength = document.querySelectorAll('.location');
+
+let position = 0;
+
+function left() {
+    position += width * step;
+    position = Math.min(position, 0);
+
+    slide.style.transform = `translateX(${position}vw)`;
+}
+
+function right(){
+    position -= width * step;
+    position = Math.max(position, -width * (locLength.length - step*imgDisplayed))
+
+    slide.style.transform = `translateX(${position}vw)`;
+}
+
+prev.addEventListener('click', left)
+next.addEventListener('click', right)
+
