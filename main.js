d4a4af02f7cfd42d31fd7984d94e818ddf60ef3e
DFKorn <d1man2192@mail.ru>
2022-03-17 23:30:18 +0300

    prevent transitions during window resizing

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let width = 29.5;
-let step = 1;
-let imgDisplayed = 3;
 
-
-let slide = document.querySelector('.slideshow');
+let slider = document.querySelector('.slideshow');
 let prev = document.querySelector('.previous');
 let next = document.querySelector('.next');
 let locLength = document.querySelectorAll('.location');
+
+//arrow controls on slideshow element
+let width = 29.5;
+let step = 1;
+let imgDisplayed = 3;
 
 let position = 0;
 
@@ -14,16 +15,31 @@ function left() {
     position += width * step;
     position = Math.min(position, 0);
 
-    slide.style.transform = `translateX(${position}vw)`;
+    /*for(const location of locLength){
+        location.style.transform = `translateX(${position}vw)`;
+    }*/
+    slider.style.transform = `translateX(${position}vw)`;
 }
 
 function right(){
     position -= width * step;
     position = Math.max(position, -width * (locLength.length - step*imgDisplayed))
 
-    slide.style.transform = `translateX(${position}vw)`;
+    /*for(const location of locLength){
+        location.style.transform = `translateX(${position}vw)`;
+    }*/
+    slider.style.transform = `translateX(${position}vw)`;
 }
 
 prev.addEventListener('click', left)
 next.addEventListener('click', right)
 
+//
+let resizeTimer;
+window.addEventListener("resize", () => {
+  slider.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    slider.classList.remove("resize-animation-stopper");
+  }, 400);
+});
