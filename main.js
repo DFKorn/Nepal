2d957962aaf245aeed1a2437bd5e0389eeb68255
DFKorn <d1man2192@mail.ru>
2022-03-19 00:18:45 +0300

    add scroll-snap for slideshow

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 
-let slider = document.querySelector('.slideshow');
-let prev = document.querySelector('.previous');
-let next = document.querySelector('.next');
-let locLength = document.querySelectorAll('.location');
+const slider = document.querySelector('.slideshow');
+const prev = document.querySelector('.previous');
+const next = document.querySelector('.next');
+const locations = document.querySelectorAll('.location');
 
-//arrow controls on slideshow element
-let width = 29.5;
+
+/*That were the first options for slideshow to work.
+One moved the slideshow element, the other moved every location element 
+Decided to leave it here just in case...*/
+//Arrow controls for slideshow element
+/*let width = 29.5;
 let step = 1;
 let imgDisplayed = 3;
 
@@ -15,31 +19,55 @@ function left() {
     position += width * step;
     position = Math.min(position, 0);
 
-    /*for(const location of locLength){
+    for(const location of locations){
         location.style.transform = `translateX(${position}vw)`;
-    }*/
-    slider.style.transform = `translateX(${position}vw)`;
+    }
+    //slider.style.transform = `translateX(${position}vw)`;
 }
 
 function right(){
     position -= width * step;
-    position = Math.max(position, -width * (locLength.length - step*imgDisplayed))
+    position = Math.max(position, -width * (locations.length - step*imgDisplayed))
 
-    /*for(const location of locLength){
+    for(const location of locations){
         location.style.transform = `translateX(${position}vw)`;
-    }*/
-    slider.style.transform = `translateX(${position}vw)`;
+    }
+    //slider.style.transform = `translateX(${position}vw)`;
 }
-
 prev.addEventListener('click', left)
 next.addEventListener('click', right)
+*/
+
+const gallery = document.querySelector('.destinations');
+const gallery_scroller = gallery.querySelector('.slideshow');
+const gallery_item_size = gallery_scroller.querySelector('.location').clientWidth;
+
+gallery.querySelector('.next').addEventListener('click', scrollToNextPage);
+gallery.querySelector('.previous').addEventListener('click', scrollToPrevPage);
+
+function scrollToNextPage() {
+  gallery_scroller.scrollBy(gallery_item_size, 0);
+}
+function scrollToPrevPage() {
+  gallery_scroller.scrollBy(-gallery_item_size, 0);
+}
+/*
+function scrollToNextPage() {
+  gallery_scroller.scrollBy({left: gallery_item_size,
+  behavior: 'smooth'});
+}
+function scrollToPrevPage() {
+  gallery_scroller.scrollBy({left: -gallery_item_size,
+  behavior: 'smooth'});
+}*/
+
 
 //
-let resizeTimer;
+/*let resizeTimer;
 window.addEventListener("resize", () => {
   slider.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(() => {
     slider.classList.remove("resize-animation-stopper");
   }, 400);
-});
+});*/
